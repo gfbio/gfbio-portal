fd9bd51401f6125157d1cc4a6e02f16251dc8b05
Kobkaew <Kobkaew@gmail.com>
2014-06-19 12:10:49 +0200

    add tagCloud

---
+++ liferay-plugins-sdk-6.2.0/portlets/GFBio_Search_Demo_1_v1-portlet/docroot/WEB-INF/src/gfbio/portal/search/tagCloud.java
@@ -0,0 +1,11 @@
+package gfbio.portal.search;
+
+import com.liferay.util.bridges.mvc.MVCPortlet;
+
+/**
+ * Portlet implementation class tagCloud
+ */
+public class tagCloud extends MVCPortlet {
+ 
+
+}
