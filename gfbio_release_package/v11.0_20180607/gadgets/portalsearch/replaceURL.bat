d5e51c2e5106caf2997a8abab9c17c507ab93f18
Sven Thiel <sven.thiel@uni-jena.de>
2018-05-29 11:25:59 +0200

    add gadget files

---
+++ gfbio_release_package/v11.0_20180607/gadgets/portalsearch/replaceURL.bat
@@ -0,0 +1,17 @@
+@echo off 
+    setlocal enableextensions disabledelayedexpansion
+	
+    set "search=//gfbio-db1.inf-bb.uni-jena.de/gfbioPub2"
+    set "replace=//gfbio-db1.inf-bb.uni-jena.de/gfbioGA4"
+	
+	for %%f in (*.xml) do (
+		echo %%f
+		
+		for /f "delims=" %%i in ('type "%%f" ^& break ^> "%%f" ') do (
+			set "line=%%i"
+			setlocal enabledelayedexpansion
+			set "line=!line:%search%=%replace%!"
+			>>"%%f" echo(!line!
+			endlocal
+		)
+	)
